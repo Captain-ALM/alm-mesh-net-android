eb3b0dde3106cc357b9eef6ec048c44767a4236e
Captain ALM <captainalm@captainalm.com>
2025-04-18 14:29:09 +0100

    Finish the settings fragment.

--- app/src/main/java/com/captainalm/mesh/MainActivity.java
+++ app/src/main/java/com/captainalm/mesh/MainActivity.java
@@ -2,7 +2,6 @@ package com.captainalm.mesh;
 
 import android.os.Bundle;
 import android.view.View;
-import android.view.Menu;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
@@ -48,13 +47,6 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
     }
 
     @Override
